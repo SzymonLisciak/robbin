33be0f62eecfc7587c7584805e5d2394c229c865
SzymonLisciak <155400528+SzymonLisciak@users.noreply.github.com>
2024-10-16 11:34:53 +0200

    Update Main.java

--- robbin/src/main/java/org/example/Main.java
+++ robbin/src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
             JFrame frame = new JFrame();
-            frame.setTitle("Nigga Simulator");
+            frame.setTitle("Simulator");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             App app = new App();
             frame.add(app);
